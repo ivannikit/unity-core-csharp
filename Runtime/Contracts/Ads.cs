1982de90e2e2fb39348b74b6e0a6b2f0a1a7fba0
Ivan <ivannikit16@gmail.com>
2020-08-28 17:16:54 +0300

    Ads

--- Runtime/Contracts/Ads.cs
+++ Runtime/Contracts/Ads.cs
@@ -1,26 +1,42 @@
-﻿namespace TeamZero.Core.Ads
+﻿using System;
+
+namespace TeamZero.Core.Ads
 {
     public interface IAdNetwork
     {
         string GetName();
         string GetId();
+
+        bool IsAvailableBanner();
+        bool IsAvailableInterstitial();
+        bool IsAvailableRewardedAd();
+    }
+
+    public interface IAdProvider
+    {
+        bool IsLoaded();
+        void Load();
+        event Action OnEndLoad;
     }
     
-    public interface IAdBannerProvider
+    public interface IAdBannerProvider : IAdProvider
     {
         void ShowBannerAd();
         void HideBannerAd();
     }
 
     public delegate void InterstitialResult();
-    public interface IAdInterstitialProvider
+    public interface IAdInterstitialProvider : IAdProvider
     {
-        void ShowInterstitialAd(InterstitialResult result);
+        event Action OnEndAd;
+        bool ShowInterstitialAd();
     }
     
     public delegate void RewardedResult(bool watched);
-    public interface IAdRewardedProvider
+    public interface IAdRewardedProvider : IAdProvider
     {
-        void ShowRewardedAd(RewardedResult result);
+        event Action OnEndAd;
+        event Action OnWatchedAd;
+        bool ShowRewardedAd();
     }
 }
