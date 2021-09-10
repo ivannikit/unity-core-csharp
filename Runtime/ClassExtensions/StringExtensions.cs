58b633d87d9ff9df7acd2646441e3924eab83fbd
Ivan <ivannikit16@gmail.com>
2021-09-10 13:43:51 +0300

    StringExtensions: add FirstCharToUpper method

--- Runtime/ClassExtensions/StringExtensions.cs
+++ Runtime/ClassExtensions/StringExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace Toolbox
+﻿using System;
+using System.Linq;
+
+namespace Toolbox
 {
     public static class StringExtensions
     {
+        public static string FirstCharToUpper(this string value)
+        {
+            switch (value)
+            {
+                case null: throw new ArgumentNullException(nameof(value));
+                case "": throw new ArgumentException($"{nameof(value)} cannot be empty", nameof(value));
+                default: return value.First().ToString().ToUpper() + value.Substring(1);
+            }
+        }
     }
 }
