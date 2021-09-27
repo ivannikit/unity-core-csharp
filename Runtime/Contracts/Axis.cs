2a5a8e6183837d09c026f629f669a84ba1f95264
Ivan <ivannikit16@gmail.com>
2021-09-27 14:27:10 +0300

    add Axis

---
+++ Runtime/Contracts/Axis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TeamZero.Core
+{
+    [Flags]
+    public enum Axis3
+    {
+        None = 0,
+        All = ~None,
+        X = 1 << 0,
+        Y = 1 << 1,
+        Z = 1 << 2,
+        XY = X | Y
+    }
+
+    [Flags]
+    public enum Axis2
+    {
+        None = 0,
+        All = ~None,
+        X = 1 << 0,
+        Y = 1 << 1,
+        XY = X | Y
+    }
+}
